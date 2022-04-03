c392ea10f0871baf4b3c7a587b4dc593090fc8b4
张小飞 <2624970078@qq.com>
2022-04-03 15:13:50 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,30 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import Home from '../components/home'
-import About from '../components/about'
-import User from '../components/user'
+// import Home from '../components/home'
+// import About from '../components/about'
+// import User from '../components/user'
 const router = new VueRouter({
     //配置路径和组件之间的映射关系
     routes:[
     {
         path: '/',
-        redirect:'/home'
+        // redirect:'/home'
+        component: () => import('../components/home'),//懒加载
     },
     {
         path: '/home',
-        component:Home,
+        // component:Home,
+        component: () => import('../components/home'),
         linkActiveClass:'active'
     },{
         path: '/about',
-        component:About
+        component: () => import('../components/about'),
+        // component:About
     },{
         path: '/user/:userId',
-        component: User
+        // component: User
+        component: () => import('../components/user')
     }
 
     ],
