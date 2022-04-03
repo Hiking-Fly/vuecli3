777bfab58f88ee08defd129884cefef43f313120
张小飞 <2624970078@qq.com>
2022-04-03 15:18:05 +0800

    路由懒加载，集中式管理

--- src/router/index.js
+++ src/router/index.js
@@ -7,27 +7,30 @@ Vue.use(VueRouter)
 // import Home from '../components/home'
 // import About from '../components/about'
 // import User from '../components/user'
+const Home = () => import('../components/home')
+const About = () => import('../components/about')
+const User = () => import('../components/user')
 const router = new VueRouter({
     //配置路径和组件之间的映射关系
     routes:[
     {
         path: '/',
         // redirect:'/home'
-        component: () => import('../components/home'),//懒加载
+        component: Home,//懒加载
     },
     {
         path: '/home',
         // component:Home,
-        component: () => import('../components/home'),
+        component: Home,
         linkActiveClass:'active'
     },{
         path: '/about',
-        component: () => import('../components/about'),
+        component: About,
         // component:About
     },{
         path: '/user/:userId',
         // component: User
-        component: () => import('../components/user')
+        component: User
     }
 
     ],
